1e8d39417828f06ccd2e625f0dbe2243e3101bc3
Shao <shaotang.yen@gmail.com>
2021-10-05 20:29:19 +1100

    　updating js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ var userFormEl = document.querySelector('#search-form');
 var userInputEl = document.querySelector('#search-input');
 var searchDisplayEl = document.querySelector('#search-display-list');
 var artistName = "";
+var artistBio = "";
+var displayResult = document.querySelector("#display-result");
 
 // API: https://api.artic.edu/docs/#introduction
 // Check out Endpoints for the Search function
@@ -58,17 +60,8 @@ function renderArtworkResult(searchResultArray) {
             console.log("Artwork Result:");
             console.log(response);
             artistName = response.data[0].artist_title;
-            var artistBio = getArtistWikiBio(artistName);
+            artistBio = getArtistWikiBio(artistName);
             
-            //display artist name and nio
-            // var displayArtistNameItem = document.createElement('li');
-            // displayArtistNameItem.textContent = "Artist: "+artistName;
-            // var displayArtistBioItem = document.createElement('li');
-            // displayArtistBioItem.textContent = "Bio "+artistBio;
-
-            // searchDisplayEl.appendChild(displayArtistNameItem);
-            // searchDisplayEl.appendChild(displayArtistBioItem);
-
             //display images
             for (i = 0; i < response.data.length; i++) {
                 var imgTitle = response.data[i].title;
@@ -92,7 +85,7 @@ function renderArtworkResult(searchResultArray) {
 
 // API: Wiki API
 function getArtistWikiBio(artistName) {
-    //https://stackoverflow.com/questions/4452102/how-to-get-plain-text-out-of-wikipedia
+    //tutorial: https://stackoverflow.com/questions/4452102/how-to-get-plain-text-out-of-wikipedia
     artistName = artistName.replace(/\s+/g, '_'); //replace space with _
     console.log("Artist Name: "+artistName);
     var searchQueryURL = "https://neon-cors-proxy.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&titles=" + artistName + "&prop=extracts&exintro&explaintext&exchars=300&formatversion=2&format=json";
@@ -117,62 +110,11 @@ function getArtistWikiBio(artistName) {
         });
 }
 
-// MetMuseum API
-//"https://neon-cors-proxy.herokuapp.com/https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&hasImages=true&artistOrCulture=true&q=Van Gogh"
-// function searchForArtist(input) {
-//     var searchQueryURL = "https://neon-cors-proxy.herokuapp.com/https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&hasImages=true&artistOrCulture=true&q=";
-//     var searchResult = [];
-//     searchQueryURL += input;
-//     fetch(searchQueryURL)
-//         .then(function (response) {
-//             if (!response.ok) {
-//                 console.log("Error:" + response.status);
-//                 throw response.json();
-//             }
-//             return response.json();
-//         })
-//         .then(function (response) {
-//             searchResult = response.objectIDs;
-//             console.log(searchResult);
-//             result1(searchResult);
-//         })
-//         .catch(function (error) {
-
-//             alert("Error Message 1: " + error);
-//         });
-// }
-
-// function result1(searchResult) {
-//     for (i = 0; i < searchResult.length; i++) {
-//         var resultQueryURL = "https://neon-cors-proxy.herokuapp.com/https://collectionapi.metmuseum.org/public/collection/v1/objects/";
-//         resultQueryURL += searchResult[i];
-//         //console.log(resultQueryURL);
-//         fetch(resultQueryURL)
-//             .then(function (response) {
-//                 if (!response.ok) {
-//                     console.log("Error:" + response.status);
-//                     throw response.json();
-//                 }
-//                 return response.json();
-//             })
-//             .then(function (response) {
-//                 console.log("Artwork");
-//                 console.log(response);
-//             })
-//             .catch(function (error) {
-
-//                 alert("Error Message 2: " + error);
-//             });
-//     }
-
-// }
-
 function formSubmitHandler(event) {
     event.preventDefault();
 
     var input = userInputEl.value.trim();
     if (input) {
-        //input = capitalizeAString(input);
         console.log(input);
         searchArtist(input);
     } else {
@@ -180,6 +122,21 @@ function formSubmitHandler(event) {
     }
 }
 
-userFormEl.addEventListener('submit', formSubmitHandler);
+function renderResult(){
+    var titleHTML = '<h1 id="result-artist-title">'+artistName+'</h1>';
+    var bioHTML = '<h2 id="result-artist-bio">'+artistBio+'</h2>';
 
-//searchForArtist();
+    
+    displayResult.appendChild(titleHTML);
+    displayResult.appendChild(bioHTML);
+
+    displayResult.style.display = "block";
+}
+
+function init(){
+    displayResult.style.display = "none";
+}
+
+init();
+
+userFormEl.addEventListener('submit', formSubmitHandler);
